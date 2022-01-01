529637b1419c57b06753f52a59c4cd7ce18cb991
faithchan <fch@mercantilemaritime.com>
2022-01-02 01:05:48 +1100

    using the params to filter and find data

--- app.js
+++ app.js
@@ -8,12 +8,22 @@ app.get('/', (req,res)=>{
     res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
 })
 
+//filter information
 app.get('/api/products', (req,res)=>{
     const newProducts = products.map((product)=>{
         const {id, name, image} = product;
     return {id,name, image}})  
     res.json(newProducts)
 })
+
+//filter futher via Id, get single product
+app.get('/api/products/:productId', (req,res)=>{
+    console.log(req.params)
+   const singleProduct = products.find((product)=>product.id === Number(req.params.productId))
+   //You can refactor Number(req.params.productId) => const {productId} = Number(req.params)
+   res.json(singleProduct)
+})
+
 
 
 app.listen(4000, ()=>{console.log("app is listening on port 4000" )})
