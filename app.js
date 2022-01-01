7c2ced8ac7a3b2ea7d4de1ce75aff964ecc020ea
faithchan <fch@mercantilemaritime.com>
2022-01-02 00:02:23 +1100

    using express.static

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ const path = require('path')
 app.use(express.static("./public"))
 
 
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
-// })
-
-
 app.all('*', (req,res)=>{res.status(404).send("<h1>resource not found</h1>")})
 
 
