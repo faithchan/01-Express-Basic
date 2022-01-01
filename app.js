cac498b8d5ef3d410b7703c4c322df71c5f7f08e
faithchan <fch@mercantilemaritime.com>
2022-01-02 00:11:04 +1100

    importing data from another file to create the api

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const {products} =require('./data')
 
 //Create first API
 app.get('/', (req,res)=>{
-    res.json([{name:"john"},{name:"susan"}])
+    res.json(products)
 })
 
 app.listen(4000, ()=>{console.log("app is listening on port 4000" )})
