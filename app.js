d6578948a599c958f5a85c5790e082f695e02c33
faithchan <fch@mercantilemaritime.com>
2022-01-02 09:48:35 +1100

    Delete Method - find by Id and delete

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express();
 let {people} = require('./data')
 
+//Example of Post 
+app.post('/login', (req,res)=>{
+    console.log(req.body)
+    const {name} = req.body;
+    if(name){return res.status(200).send(`welcome ${name}`)}
+    res.status(401).send('Please provide credentials')
+})
+
 //static
 app.use(express.static('./methods-public'))
 
@@ -15,13 +23,6 @@ app.use(express.urlencoded({extended:false}))
 //patse json
 app.use(express.json())
 
-//Example of Post 
-app.post('/login', (req,res)=>{
-    console.log(req.body)
-    const {name} = req.body;
-    if(name){return res.status(200).send(`welcome ${name}`)}
-    res.status(401).send('Please provide credentials')
-})
 
 //add people
 app.post('/api/people', (req,res)=>{
@@ -54,6 +55,20 @@ const newPeople = people.map((person)=>{
 })
 res.status(200).json({success:true, data:newPeople})
 })
+
+//delete
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if (!person) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `no person with id ${req.params.id}` })
+    }
+    const newPeople = people.filter(
+      (person) => person.id !== Number(req.params.id)
+    )
+    return res.status(200).json({ success: true, data: newPeople })
+  })
 
 app.listen(4000, ()=>{console.log("app is listening on port 4000" )})
 
