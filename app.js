4d9163f863b5e106a80a635df7871785531951ae
faithchan <fch@mercantilemaritime.com>
2022-01-02 02:21:11 +1100

    basic example of middleware

--- app.js
+++ app.js
@@ -1,48 +1,23 @@
 const express = require('express')
 const app = express();
-const path = require('path')
-const {products} =require('./data')
+//req = > middleware => res
 
-//sending html
-app.get('/', (req,res)=>{
-    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+//Middleware
+const yearLog =(req, res,next)=>{    
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method, url, time)
+// res.send('testing')}
+next();}
+
+//add middleware manually here as a parameter
+app.get('/', yearLog ,(req,res)=>{
+
+    res.send('Home')
 })
-
-//filter information
-app.get('/api/products', (req,res)=>{
-    const newProducts = products.map((product)=>{
-        const {id, name, image} = product;
-    return {id,name, image}})  
-    res.json(newProducts)
-})
-
-//filter futher via Id, get single product
-app.get('/api/products/:productId', (req,res)=>{
-    console.log(req.params)
-   const singleProduct = products.find((product)=>product.id === Number(req.params.productId)
-   )
-   //You can refactor Number(req.params.productId) => const {productId} = Number(req.params)
-   if(!singleProduct){return res.status(404).send("product not found")}
-   res.json(singleProduct)
-})
-
-//query
-app.get('/api/v1/query', (req,res)=>{
-    //you have to include a ? after the url and you can add a & to query as many things
-    const {search, limit} = req.query;
-    let sortedProducts= [...products];
-
-    if(search){sortedProducts= sortedProducts.filter((product)=>{
-        return product.name.startsWith(search)
-    })}
-
-    if(limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-
-    if(sortedProducts.length<1){res.status(200).send('No products matched description')}
-
-    res.status(200).json(sortedProducts)
+app.get('/about',(req,res)=>{
+    res.send('About')
 })
 
 
