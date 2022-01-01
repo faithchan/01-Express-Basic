541cbefd4a55aade6ecf90ba05c5839857b95289
faithchan <fch@mercantilemaritime.com>
2022-01-02 07:54:05 +1100

    Idea of authorization in a middleware

--- authorize.js
+++ authorize.js
@@ -1,6 +1,14 @@
 const authorize =(req,res,next)=>{
-    console.log('authorize')
-    next();
+    const {user} = req.query
+    if(user ==='susan'){
+        req.user = {name:'susan', id:2}
+        next()
+    }
+    else {
+        res.status(401).send('unauthorized')
+    }
+
 }
+//After url add ?user=susan =>turn into authorize and you can visit the pages again
 
 module.exports= authorize
