1bd1fe3ad353206585400e59f8fab5e83386e37e
faithchan <fch@mercantilemaritime.com>
2022-01-02 00:50:38 +1100

    Extracting only required information from the api and upon click on href, return the json data

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const app = express();
 const path = require('path')
 const {products} =require('./data')
 
-//Create first API
+//sending html
 app.get('/', (req,res)=>{
-    res.json(products)
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
 })
+
+app.get('/api/products', (req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id, name, image} = product;
+    return {id,name, image}})  
+    res.json(newProducts)
+})
+
 
 app.listen(4000, ()=>{console.log("app is listening on port 4000" )})
 
