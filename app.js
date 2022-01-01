eb8c8d7834609c7da5a544dd9536513bc2744604
faithchan <fch@mercantilemaritime.com>
2022-01-02 00:01:07 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+//set up static and middleware
+app.use(express.static("./public"))
+
+
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
+// })
+
+
+app.all('*', (req,res)=>{res.status(404).send("<h1>resource not found</h1>")})
+
+
+app.listen(4000, ()=>{console.log("app is listening on port 4000" )})
+
