230dbc8ce461dd35bbfa67dd52f27b013f32db60
faithchan <fch@mercantilemaritime.com>
2022-01-02 08:09:04 +1100

    GET Method

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
 const express = require('express')
 const app = express();
-const yearLog = require('./logger')
-const authorize = require('./authorize')
-const morgan = require('morgan')
-//req = > middleware => res
+let {people} = require('./data')
 
-// Options of middleware - our own / express / third party
-//express
-// app.use(express.static('./public')) -express
-
-//third party
-app.use(morgan('tiny'))
-
-// app.use([yearLog, authorize]) -our own
-
-app.get('/',(req,res)=>{
-
-    res.send('Home')
-})
-
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-
-//example of passing two middlewares into a specific route
-app.get('/api/products',(req,res)=>{
-    res.send('products')
-})
-
-app.get('/api/items',(req,res)=>{
-    res.send('Items')
+app.get('/api/people', (req,res)=>{
+    res.status(200).json({success:true, data:people})
 })
 
 
