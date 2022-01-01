dcb4f6561db96dd06e42e90fd845584bd2933747
faithchan <fch@mercantilemaritime.com>
2022-01-02 08:47:40 +1100

    javascript form, add names

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get('/api/people', (req,res)=>{
 //parse form data
 app.use(express.urlencoded({extended:false}))
 
+//patse json
+app.use(express.json())
+
 //Example of Post 
 app.post('/login', (req,res)=>{
     console.log(req.body)
@@ -20,7 +23,13 @@ app.post('/login', (req,res)=>{
     res.status(401).send('Please provide credentials')
 })
 
-//
+//add people
+app.post('/api/people', (req,res)=>{
+    const {name} = req.body
+    if(!name){return res.status(400).send({success:false, msg:"Please provide name"})}
+    res.status(201).send({success:true,person:name})
+})
+
 
 app.listen(4000, ()=>{console.log("app is listening on port 4000" )})
 
