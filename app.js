aad3b4d56109584b5261f731f7c2870355433819
faithchan <fch@mercantilemaritime.com>
2022-01-02 08:19:49 +1100

    Post Method - Example

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express();
 let {people} = require('./data')
 
+//static
+app.use(express.static('./methods-public'))
+
 app.get('/api/people', (req,res)=>{
     res.status(200).json({success:true, data:people})
 })
 
+//Example of Post 
+app.post('/login', (req,res)=>{
+    res.send('POST')
+})
+
+//
 
 app.listen(4000, ()=>{console.log("app is listening on port 4000" )})
 
