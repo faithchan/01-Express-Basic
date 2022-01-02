b621d82b664b7fabdab70e9dd4d9ae3147fbe6f6
faithchan <fch@mercantilemaritime.com>
2022-01-02 22:12:03 +1100

    create controllers step 1

--- routes/people.js
+++ routes/people.js
@@ -2,53 +2,17 @@ const express = require('express')
 const router =express.Router();
 let {people} = require('../data')
 
-router.get('/', (req,res)=>{
-    res.status(200).json({success:true, data:people})
-})
+router.get('/', )
 //add people
-router.post('/', (req,res)=>{
-    const {name} = req.body
-    if(!name){return res.status(400).send({success:false, msg:"Please provide name"})}
-    res.status(201).send({success:true,person:name})
-})
+router.post('/', )
 
 //add people to the array using postman
-router.post('/postman', (req,res)=>{
-    const {name} = req.body
-    if(!name){return res.status(400).send({success:false, msg:"Please provide name"})}
-    res.status(201).send({success:true,data:[...people,{name}]})
-})
+router.post('/postman', )
 
 //update data of one person
-router.put('/:id', (req,res)=>{
-const {id} =req.params
-const {name} = req.body
-const person = people.find((person)=>{
-    person.id === Number(id)
-
-    if(!person){return res.status(400).json({success:false, msg:"No person with Id"})}
-})
-const newPeople = people.map((person)=>{
-    if (person.id === Number(id)){
-        person.name = name
-    }
-    return person
-})
-res.status(200).json({success:true, data:newPeople})
-})
+router.put('/:id', )
 
 //delete
-router.delete('/:id', (req, res) => {
-    const person = people.find((person) => person.id === Number(req.params.id))
-    if (!person) {
-      return res
-        .status(404)
-        .json({ success: false, msg: `no person with id ${req.params.id}` })
-    }
-    const newPeople = people.filter(
-      (person) => person.id !== Number(req.params.id)
-    )
-    return res.status(200).json({ success: true, data: newPeople })
-  })
+router.delete('/:id', )
 
   module.exports =router
