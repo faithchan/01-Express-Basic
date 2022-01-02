f481ca3a23a0215b3757a4ae97547a603984d0a8
faithchan <fch@mercantilemaritime.com>
2022-01-02 22:13:35 +1100

    controllers refactor DONE

--- routes/people.js
+++ routes/people.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const router =express.Router();
 let {people} = require('../data')
+const {
+    getPeople, createPerson, updatePerson, createPersonPostman,deletePerson
+} = require('../controllers/people')
 
-router.get('/', )
+router.get('/',getPeople )
 //add people
-router.post('/', )
+router.post('/',createPerson )
 
 //add people to the array using postman
-router.post('/postman', )
+router.post('/postman', createPersonPostman )
 
 //update data of one person
-router.put('/:id', )
+router.put('/:id',updatePerson )
 
 //delete
-router.delete('/:id', )
+router.delete('/:id', deletePerson)
 
   module.exports =router
