e58a245e4e6660b0973447dd7f0d1d3ca2d8b583
faithchan <fch@mercantilemaritime.com>
2022-01-02 22:16:03 +1100

    chaining those who routes are the same - refactoring controllers

--- routes/people.js
+++ routes/people.js
@@ -5,17 +5,22 @@ const {
     getPeople, createPerson, updatePerson, createPersonPostman,deletePerson
 } = require('../controllers/people')
 
-router.get('/',getPeople )
-//add people
-router.post('/',createPerson )
+// router.get('/',getPeople )
+// //add people
+// router.post('/',createPerson )
 
-//add people to the array using postman
-router.post('/postman', createPersonPostman )
+// //add people to the array using postman
+// router.post('/postman', createPersonPostman )
 
-//update data of one person
-router.put('/:id',updatePerson )
+// //update data of one person
+// router.put('/:id',updatePerson )
 
-//delete
-router.delete('/:id', deletePerson)
+// //delete
+// router.delete('/:id', deletePerson)
+
+//chaining
+router.route('/').get(getPeople).post(createPerson)
+router.route('/postman').post(createPersonPostman)
+router.route('/:id').put(updatePerson).delete(deletePerson)
 
   module.exports =router
